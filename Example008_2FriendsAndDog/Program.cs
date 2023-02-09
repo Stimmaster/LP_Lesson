317446037ca8c8bda0e99a7c18ae259923b04a5c
Stimmaster <stimmaster@gmail.com>
2023-02-09 14:33:08 +0300

    add Example008_2FriendsAndDog

---
+++ Example008_2FriendsAndDog/Program.cs
@@ -0,0 +1,28 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+bool friend = true;
+int time = 0;
+
+while(distance > 10)
+{
+    if(friend == true)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = true;        
+    }
+
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = false;
+    }
+    
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count = count + 1;
+}
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз.");
